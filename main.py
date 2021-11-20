d8bf561088cd39287690db3c9d4c1e44751979e6
JeremyVunCode4Fun <94739483+JeremyVun@users.noreply.github.com>
2021-11-20 16:28:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Your program should print the below
+
+# Charles Dickens died in 1870
+# William Thackeray died in 1863
+# Anthony Trollope died in 1882
+# Gerard Manley Hopkins died in 1889
+
+authrs = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"
+
+for author date in authors.items{}:
+    print(f"{authors} died in {Date}")
+}
